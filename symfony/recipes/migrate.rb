9d922e324f1533c22b3eaaebd7cbe2b3f358b8f6
asquel <axel.barbier@ugroupmedia.com>
2015-04-29 10:57:20 -0600

    doctrine migrate no interaction

--- symfony/recipes/migrate.rb
+++ symfony/recipes/migrate.rb
@@ -7,7 +7,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    php app/console doctrine:migrations:migrate --env="#{node[:symfony][:environment]}
+    php app/console doctrine:migrations:migrate --no-interaction --env="#{node[:symfony][:environment]}
     EOH
   end
 end 
