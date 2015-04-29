c7a9f81827de3d10dee56319b75aefe63a2a3ea9
asquel <axel.barbier@ugroupmedia.com>
2015-04-29 14:20:23 -0600

    update default

---
+++ phpmyadmin/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:phpmyadmin][:cfg][:cfg_path]               =   '/etc/phpmyadmin'
+default[:phpmyadmin][:cfg][:path]                   =   '/usr/share/phpmyadmin'
+default[:phpmyadmin][:apache2][:site_config]        =   '/etc/phpmyadmin/apache.conf'
