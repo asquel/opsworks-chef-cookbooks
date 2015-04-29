f6c09177348bd4ceb24150b1734f2a8733e5b998
asquel <axel.barbier@ugroupmedia.com>
2015-04-29 12:57:20 -0600

    update with conf

--- symfony/recipes/migrate.rb
+++ symfony/recipes/migrate.rb
@@ -7,10 +7,10 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    export SYMFONY__DATABASE__HOST
-    export SYMFONY__DATABASE__NAME
-    export SYMFONY__DATABASE__USER
-    export SYMFONY__DATABASE__PASSWORD
+    export SYMFONY__DATABASE__HOST=#{node[:symfony][:database_host]}
+    export SYMFONY__DATABASE__NAME=#{node[:symfony][:database_name]}
+    export SYMFONY__DATABASE__USER=#{node[:symfony][:database_user]}
+    export SYMFONY__DATABASE__PASSWORD=#{node[:symfony][:database_password]}
     php app/console doctrine:migrations:migrate --no-interaction --env="#{node[:symfony][:environment]}
     EOH
   end
